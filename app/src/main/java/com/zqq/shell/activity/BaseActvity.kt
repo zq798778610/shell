a8ccbdb8dbfb3d15fea0248ef0e491b342fb3499
zqq <qweqweqw@163.com>
2021-03-29 16:42:59 +0800

    设置 BaseActvity.kt

--- app/src/main/java/com/zqq/shell/activity/BaseActvity.kt
+++ app/src/main/java/com/zqq/shell/activity/BaseActvity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.zqq.shell.MyApplication
+import com.zqq.shell.R
+import com.zqq.shell.utils.StatusBarUtil
 
 open abstract class BaseActvity :AppCompatActivity() {
 
@@ -21,6 +23,9 @@ open abstract class BaseActvity :AppCompatActivity() {
         val contentView = initContentView() //设置contentView
         setContentView(contentView)
 
+        //依赖 implementation 'com.readystatesoftware.systembartint:systembartint:1.0.3' //状态栏颜色改变
+        StatusBarUtil.setStatusBarColor(this, R.color.main_color) //设置状态栏颜色
+
         app = MyApplication.instance()
         app.addActivity(this)
 
@@ -31,4 +36,11 @@ open abstract class BaseActvity :AppCompatActivity() {
     abstract fun initContentView():Int
     abstract fun initView(savedInstanceState: Bundle?)
     abstract fun initData()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        app.removeActivity(this)
+    }
+
+
 }
--- app/src/main/java/com/zqq/shell/activity/SplashActvity.kt
+++ app/src/main/java/com/zqq/shell/activity/SplashActvity.kt
@@ -9,10 +9,8 @@ class SplashActvity :BaseActvity() {
     override fun initContentView(): Int = R.layout.activity_splash
 
     override fun initView(savedInstanceState: Bundle?) {
-        val currentActivity = app.getCurrentActivity()
-        Log.i("zqqqq",currentActivity.localClassName)
-
-       startActivity(Intent(this,MainActivity::class.java))
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
     }
 
     override fun initData() {
