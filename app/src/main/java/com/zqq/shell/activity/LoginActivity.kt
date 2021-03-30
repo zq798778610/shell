eb9efaa95d67d9cc193e56d54d79276ec9936da6
zqq <qweqweqw@163.com>
2021-03-30 14:53:45 +0800

    权限

--- app/src/main/java/com/zqq/shell/activity/LoginActivity.kt
+++ app/src/main/java/com/zqq/shell/activity/LoginActivity.kt
@@ -1,10 +1,16 @@
 package com.zqq.shell.activity
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.EditText
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import butterknife.BindView
 import com.zqq.shell.R
 import com.zqq.shell.view.EditTextLayout
+
 
 class LoginActivity :BaseActvity() {
 
@@ -15,6 +21,9 @@ class LoginActivity :BaseActvity() {
     lateinit var el_password: EditTextLayout
 
 
+    val permissions = arrayOf(Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_PHONE_STATE)
+
     override fun initContentView(): Int = R.layout.activity_login
 
     override fun initView(savedInstanceState: Bundle?) {
@@ -22,6 +31,26 @@ class LoginActivity :BaseActvity() {
     }
 
     override fun initData() {
+        val needPermissions = ArrayList<String>()
 
+        for (index in permissions.indices){
+            val checkSelfPermission = ContextCompat.checkSelfPermission(applicationContext, permissions[index])
+            Log.i("zqqq","checkSelfPermission>>$checkSelfPermission")
+            if(checkSelfPermission == PackageManager.PERMISSION_DENIED) {
+                needPermissions.add(permissions[index])
+            }
+        }
+
+        if(needPermissions.size>0){
+            ActivityCompat.requestPermissions(this, permissions, 10001)  //请求权限
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(data == null) return
+        if(requestCode == 10001){
+
+        }
     }
 }
